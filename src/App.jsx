5441f0d3a2672f78ceaf39b64052b19cc06c63ff
yossi_16 <ylerer770@gmail.com>
2026-03-08 22:03:02 +0200

    fix date

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ export default function App() {
 
   // הגדרת 8 הימים האחרונים עם תאריכי פתיחה ספציפיים
   const [cards] = useState([
-    { id: 10, date: "2026-03-06T00:00:00", img: '/image/PH-10.jpeg' },
-    { id: 9, date: "2026-03-07T00:00:00", img: "/image/PH-9.jpeg" },
-    { id: 8, date: "2026-03-08T00:00:00", img: "/image/PH-8.jpeg" },
-    { id: 7, date: "2026-03-09T00:00:00", img: "/image/PH-7.jpeg" },
-    { id: 6, date: "2026-03-10T00:00:00", img: "/image/PH-6.jpeg" },
-    { id: 5, date: "2026-03-11T00:00:00", img: "/image/PH-5.jpeg" },
-    { id: 4, date: "2026-03-12T00:00:00", img: "/image/PH-4.jpeg" },
-    { id: 3, date: "2026-03-13T00:00:00", img: "/image/PH-3.jpeg" },
-    { id: 2, date: "2026-03-14T00:00:00", img: "/image/PH-2.jpeg" },
-    { id: 1, date: "2026-03-15T00:00:00", img: "/image/PH-1.jpeg" },
+    { id: 10, date: "2026-03-05T00:00:00", img: '/image/PH-10.jpeg' },
+    { id: 9, date: "2026-03-06T00:00:00", img: "/image/PH-9.jpeg" },
+    { id: 8, date: "2026-03-07T00:00:00", img: "/image/PH-8.jpeg" },
+    { id: 7, date: "2026-03-08T00:00:00", img: "/image/PH-7.jpeg" },
+    { id: 6, date: "2026-03-09T00:00:00", img: "/image/PH-6.jpeg" },
+    { id: 5, date: "2026-03-10T00:00:00", img: "/image/PH-5.jpeg" },
+    { id: 4, date: "2026-03-11T00:00:00", img: "/image/PH-4.jpeg" },
+    { id: 3, date: "2026-03-12T00:00:00", img: "/image/PH-3.jpeg" },
+    { id: 2, date: "2026-03-13T00:00:00", img: "/image/PH-2.jpeg" },
+    { id: 1, date: "2026-03-14T00:00:00", img: "/image/PH-1.jpeg" },
   ]);
 
   
